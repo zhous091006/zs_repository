4962604dcb6ee68e3176d531b1563ccd01e61788
zhous091006 <127010580+zhous091006@users.noreply.github.com>
2026-07-23 11:45:47 +0800

    Update test_push_button.py
    Update test_push_button.py

--- test_push_button.py
+++ test_push_button.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
-
 from ui.custom_widgets.buttons.ui_push_button import UiPushButton
+from ui.custom_widgets.inputs.ui_serial_number_edit import UiSerialNumberEdit
 
 app = QApplication(sys.argv)
 
@@ -15,8 +15,11 @@ btn.clicked.connect(lambda: print("按钮被点击"))
 btn2 = UiPushButton.create_icon_button(text="﹣", object_name="Icon", width=20, height=20, parent=w)
 btn2.clicked.connect(lambda: print("按钮2被点击"))
 
+edit = UiSerialNumberEdit()
+
 layout.addWidget(btn)
 layout.addWidget(btn2)
+layout.addWidget(edit)
 
 w.show()
 app.exec_()
